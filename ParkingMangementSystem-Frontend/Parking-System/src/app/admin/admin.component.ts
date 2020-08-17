404ea4fd243398c577905ca178c6aa8c98bde8f9
Uma Shankar <56968738+Umashankar-live@users.noreply.github.com>
2020-08-17 11:20:53 +0530

    admin changes

--- ParkingMangementSystem-Frontend/Parking-System/src/app/admin/admin.component.ts
+++ ParkingMangementSystem-Frontend/Parking-System/src/app/admin/admin.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import {Router} from '@angular/router';
-import {AuthService} from '../service/auth.service'
+import { Router } from '@angular/router';
+import { AuthService } from '../service/auth.service'
+import { UserService } from '../service/user.service';
+import { UserModel } from '../Models/customer.model';
 
 @Component({
   selector: 'app-admin',
@@ -8,31 +10,41 @@ import {AuthService} from '../service/auth.service'
   styleUrls: ['./admin.component.css']
 })
 export class AdminComponent implements OnInit {
-
-  constructor(private route : Router , private authService : AuthService) { }
+  userList: UserModel[] = [];
+  page: number = 1;
+  constructor(private route: Router, private authService: AuthService, private userService: UserService) { }
 
   ngOnInit() {
+    setTimeout(() => { this.reloadData() }, 100);
+
+  }
+
+  reloadData() {
+    this.userService.fetchAllUser().subscribe(data => {
+      this.userList = data;
+      console.log(this.userList);
+    })
   }
 
 
   /* Methods for the Admin Portal */
-  clickOnAddParking(){
+  clickOnAddParking() {
     this.route.navigate(['add-parking']);
   }
 
-  clickOnListParking(){
+  clickOnListParking() {
     this.route.navigate(['list-parking']);
   }
 
-  clickOnListBooking(){
+  clickOnListBooking() {
     this.route.navigate(['bookinglist']);
   }
 
-  logout(){
+  logout() {
     sessionStorage.clear();
     this.authService.setLoggedIn(false);
     this.route.navigate(['login']);
-    
+
   }
 
 }
